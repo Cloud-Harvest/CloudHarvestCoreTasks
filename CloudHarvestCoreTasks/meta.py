516dd84b761a2c6749d38a1dd14360e393a13c9d
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-24 22:54:33 -0400

    refactored meta.py as a json file

--- CloudHarvestCoreTasks/meta.py
+++
@@ -1,9 +0,0 @@
-meta = dict(name="CloudHarvestCoreTasks",
-            version="0.1.0",
-            description="This is the Core Task system for CloudHarvest.",
-            author="Cloud Harvest, Fiona June Leathers",
-            license="CC Attribution-NonCommercial-ShareAlike 4.0 International",
-            url="https://github.com/Cloud-Harvest/CloudHarvestCoreTasks",
-            classifiers=[
-                'Programming Language :: Python :: 3.12',
-            ])
--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
-from CloudHarvestCoreTasks.meta import meta
 
-# Load requirements from requirements.txt
+# load the metadata from the meta.json file
+with open('meta.json') as meta_file_stream:
+    from json import load
+    meta = load(meta_file_stream)
+
+# load requirements from requirements.txt
 with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+    required = f.read().splitlines()
 
-config = dict(packages=find_packages(),
-              install_requires=requirements)
+config = dict(packages=find_packages(include=['CloudHarvestCoreTasks']),
+              install_requires=required)
 
 config = config | meta
 
